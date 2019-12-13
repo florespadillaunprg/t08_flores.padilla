4a847036ed88789f1487a24c59c8c8ba26f85af4
florespadillaunprg <57303134+florespadillaunprg@users.noreply.github.com>
2019-12-13 09:13:53 -0500

    Se agrego la funcion iteracion (olazabal)

---
+++ olazabal/6_iteracion.py
@@ -0,0 +1,442 @@
+#Ejercicio1
+mensaje="NO HAY ROSAS SIN ESPINAS"
+contador_a=0
+contador_e=0
+contador_i=0
+contador_o=0
+contador_u=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'E'):
+        contador_e+=1
+    if (letra == 'I'):
+        contador_i+=1
+    if (letra == 'O'):
+        contador_o+=1
+    if (letra == 'U'):
+        contador_u+=1
+
+# Contar cuantas vocales existen en la cadena
+print("A: ",contador_a)
+print("E: ",contador_e)
+print("I: ",contador_i)
+print("O: ",contador_o)
+print("U: ",contador_u)
+
+
+
+
+
+
+
+
+
+#Ejercicio2
+mensaje="DONDE HAY AMOR NO HAY PECADO"
+contador_d=0
+contador_c=0
+contador_h=0
+contador_y=0
+contador_n=0
+
+for letra in mensaje:
+    if (letra == 'D'):
+        contador_d+=1
+    if (letra == 'C'):
+        contador_c+=1
+    if (letra == 'H'):
+        contador_h+=1
+    if (letra == 'Y'):
+        contador_y+=1
+    if (letra == 'N'):
+        contador_n+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("D: ",contador_d)
+print("C: ",contador_c)
+print("H: ",contador_h)
+print("Y: ",contador_y)
+print("N: ",contador_n)
+
+
+
+
+
+
+#Ejercicio3
+mensaje="OJOS QUE NO VEN, CORAZON QUE NO SIENTE"
+contador_o=0
+contador_j=0
+contador_n=0
+contador_c=0
+contador_t=0
+
+for letra in mensaje:
+    if (letra == 'O'):
+        contador_o+=1
+    if (letra == 'J'):
+        contador_j+=1
+    if (letra == 'N'):
+        contador_n+=1
+    if (letra == 'C'):
+        contador_c+=1
+    if (letra == 'T'):
+        contador_t+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("O: ",contador_o)
+print("J: ",contador_j)
+print("N: ",contador_n)
+print("C: ",contador_c)
+print("T: ",contador_t)
+
+
+
+
+
+
+
+#Ejercicio4
+mensaje="DESGRACIADO EN EL JUEGO, AFORTUNAOD EN EL AMOR"
+contador_a=0
+contador_m=0
+contador_o=0
+contador_t=0
+contador_l=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'M'):
+        contador_m+=1
+    if (letra == 'O'):
+        contador_o+=1
+    if (letra == 'T'):
+        contador_t+=1
+    if (letra == 'L'):
+        contador_l+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("A: ",contador_a)
+print("M: ",contador_m)
+print("O: ",contador_o)
+print("T: ",contador_t)
+print("L: ",contador_l)
+
+
+
+
+
+
+#Ejercicio5
+mensaje="PARA TOREAR Y CASARSE, HAY QUE ARRIMARSE"
+contador_P=0
+contador_c=0
+contador_s=0
+contador_e=0
+contador_u=0
+
+for letra in mensaje:
+    if (letra == 'P'):
+        contador_P+=1
+    if (letra == 'C'):
+        contador_c+=1
+    if (letra == 'S'):
+        contador_s+=1
+    if (letra == 'E'):
+        contador_e+=1
+    if (letra == 'U'):
+        contador_u+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("P: ",contador_P)
+print("C: ",contador_c)
+print("S: ",contador_s)
+print("E: ",contador_e)
+print("U: ",contador_u)
+
+
+
+
+
+#Ejercicio6
+mensaje="TODO ESTA PERMITIDO EN EL AMOR"
+contador_a=0
+contador_m=0
+contador_o=0
+contador_r=0
+contador_t=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'M'):
+        contador_m=1
+    if (letra == 'O'):
+        contador_o+=1
+    if (letra == 'R'):
+        contador_r+=1
+    if (letra == 'T'):
+        contador_t+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("A: ",contador_a)
+print("M: ",contador_m)
+print("O: ",contador_o)
+print("R: ",contador_r)
+print("T: ",contador_t)
+
+
+
+
+
+
+#Ejercicio7
+mensaje="NO HAY LUNA COMO LA DE ENERO NI AMOR COMO EL PRIMERO"
+contador_a=0
+contador_e=0
+contador_i=0
+contador_o=0
+contador_u=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'E'):
+        contador_e+=1
+    if (letra == 'I'):
+        contador_i+=1
+    if (letra == 'O'):
+        contador_o+=1
+    if (letra == 'U'):
+        contador_u+=1
+
+# Contar cuantas vocales existen en la cadena
+print("A: ",contador_a)
+print("E: ",contador_e)
+print("I: ",contador_i)
+print("O: ",contador_o)
+print("U: ",contador_u)
+
+
+
+
+
+#Ejercicio9
+mensaje="QUE BIEN TE QUIERE, TE HARA LLORRAR"
+contador_q=0
+contador_t=0
+contador_r=0
+contador_m=0
+contador_l=0
+
+for letra in mensaje:
+    if (letra == 'Q'):
+        contador_q+=1
+    if (letra == 'T'):
+        contador_t+=1
+    if (letra == 'R'):
+        contador_r+=1
+    if (letra == 'M'):
+        contador_m+=1
+    if (letra == 'L'):
+        contador_l+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("Q: ",contador_q)
+print("T: ",contador_t)
+print("R: ",contador_r)
+print("M: ",contador_m)
+print("L: ",contador_l)
+
+
+
+
+
+#Ejercicio10
+mensaje="A AMOR Y FORTUNA, RESISTENCIA NINGUNA"
+contador_a=0
+contador_f=0
+contador_r=0
+contador_c=0
+contador_n=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'F'):
+        contador_f+=1
+    if (letra == 'R'):
+        contador_r+=1
+    if (letra == 'C'):
+        contador_c+=1
+    if (letra == 'N'):
+        contador_n+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("A: ",contador_a)
+print("F: ",contador_f)
+print("R: ",contador_r)
+print("C: ",contador_m)
+print("N: ",contador_l)
+
+
+
+
+
+
+
+#Ejercicio11
+mensaje="EL AMOR ES CIEGO, PERO EL MATRIMONIO LO CURA"
+contador_a=0
+contador_c=0
+contador_i=0
+contador_g=0
+contador_o=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'C'):
+        contador_c+=1
+    if (letra == 'I'):
+        contador_i+=1
+    if (letra == 'G'):
+        contador_g+=1
+    if (letra == 'O'):
+        contador_o+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("A: ",contador_a)
+print("C: ",contador_c)
+print("I: ",contador_i)
+print("G: ",contador_g)
+print("O: ",contador_o)
+
+
+
+
+
+#Ejercicio12
+mensaje="AMA AL AGRADO QUE QUIERAS SER AMADO"
+contador_a=0
+contador_l=0
+contador_g=0
+contador_q=0
+contador_m=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'L'):
+        contador_l+=1
+    if (letra == 'G'):
+        contador_g+=1
+    if (letra == 'Q'):
+        contador_q+=1
+    if (letra == 'M'):
+        contador_m+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("A: ",contador_a)
+print("L: ",contador_l)
+print("G: ",contador_g)
+print("Q: ",contador_q)
+print("M: ",contador_m)
+
+
+
+
+
+#Ejercicio13
+mensaje="AMOR LOCO SI ELLA ES MUCHO Y TU ERES POCO"
+contador_a=0
+contador_y=0
+contador_t=0
+contador_o=0
+contador_s=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'Y'):
+        contador_y+=1
+    if (letra == 'T'):
+        contador_t+=1
+    if (letra == 'O'):
+        contador_o+=1
+    if (letra == 'S'):
+        contador_s+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("A: ",contador_a)
+print("Y: ",contador_y)
+print("T: ",contador_t)
+print("O: ",contador_o)
+print("S: ",contador_s)
+
+
+
+
+
+
+#Ejercicio14
+mensaje="EL QUE AMA A UNA CASADA, PUEDE MORIR DE CORNADA"
+contador_a=0
+contador_c=0
+contador_m=0
+contador_o=0
+contador_d=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'C'):
+        contador_c+=1
+    if (letra == 'M'):
+        contador_m+=1
+    if (letra == 'O'):
+        contador_o+=1
+    if (letra == 'D'):
+        contador_d+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("A: ",contador_a)
+print("C: ",contador_c)
+print("M: ",contador_m)
+print("O: ",contador_o)
+print("D: ",contador_d)
+
+
+
+
+
+
+#Ejercicio15
+mensaje="AMAR SIN SER AMADO, ES COMO SER LIMPIARSE EL CULO SIN HABER CAGADO"
+contador_a=0
+contador_s=0
+contador_l=0
+contador_n=0
+contador_g=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'S'):
+        contador_s+=1
+    if (letra == 'L'):
+        contador_l+=1
+    if (letra == 'N'):
+        contador_n+=1
+    if (letra == 'G'):
+        contador_g+=1
+
+# Contar cuantas de las letras mencionada existen en la cadena
+print("A: ",contador_a)
+print("S: ",contador_s)
+print("L: ",contador_l)
+print("N: ",contador_n)
+print("G: ",contador_g)
