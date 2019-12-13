09f70aaeac7912f081503a8fc8c170c099c75a8d
florespadillaunprg <aflorespa@unprg.edu.pe>
2019-12-13 08:49:39 -0500

    Se agrego la funcion longitud

---
+++ flores/2_longitud.py
@@ -0,0 +1,159 @@
+# Ejercicio 01
+texto01="haz lo que amas, ama todo lo que hagas"
+print("Texto 01 :",texto01)
+print("len =",len(texto01))
+
+
+# Ejercicio 02
+texto02="muchas personas no gozan de las pequenas alegrias, porque esperan la gran felicidad"
+print("Texto 02: ", texto02)
+print("len =",len(texto02))
+# Del texto02, copiar "personas" y "felicidad", tambien declararlas
+personas="personas"
+felicidad="felicidad"
+print("muchas \'"+personas+"\' no gozan de las pequenas alegrias, porque esperan la gran \""+ felicidad+"\"")
+
+
+# Ejercicio 03
+texto03="aprendi que no se puede dar marcha atras, que la esencia de la vida es ir hacia adelante"
+print("len =",len(texto03))
+# Del texto03, copiar 'aprendi' y "escencia"
+# Declarar variables: (a="aprendi") y (e="escencia")
+a="aprendi"
+e="escencia"
+print("\'"+a+"\' que no se puede dar marcha atras, que la \""+e+"\" de la vida es ir hacia adelante")
+
+
+# Ejercicio 04
+texto04="tan pronto como uno es infeliz, uno se convierte en moralista"
+print("Texto 04 :",texto04)
+print("len =",len(texto04))
+# Del texto04, copiar "pronto" y "moralisat"
+# Declarar variables: (p="pronto") y (m="moralista")
+p="pronto"
+m="moralista"
+print("tan \""+p+"\" como uno es infeliz, uno se convierte en \""+m+"\"")
+
+
+# Ejercicio 05
+texto05="la vida no se mide por las veces que respiras, sino por los momentos que te dejan sin aliento"
+print("Texto 05 :",texto05)
+print("len =",len(texto05))
+# Del texto05, copiar 'respiras' y 'aliento'
+# Declarar variables: (r="respiras") y (aliento="aliento)
+r="respiras"
+aliento="aliento"
+print("la vida no se mide por las veces que \'"+r+"\'"", sino por los momentos que te dejan sin \'"+aliento+"\'")
+
+
+# Ejercicio 06
+texto06="cumplamos la tarea de vivir de tal modo que cuando moramos, incluso el de la funeraria lo sienta"
+print("Texto 06 :",texto06)
+print("len =",len(texto06))
+# Del texto06, copiar "cumplamos" y 'funeraria'
+# Declarar variables: (c="cumplamos) y (f="funeraria")
+c="cumplamos"
+f="funeraria"
+print("\""+c+"\" la tarea de vivir de tal modo que cuando moramos, incluso el de la \'"+f+"\' lo sienta")
+
+
+# Ejercicio 07
+texto07="una persona que ve la vida a los 50 anios igual que a los 20, ha desperdiciado 30 anios de vida"
+print("Texto 07 :",texto07)
+print("len =",len(texto07))
+# Del texto07, copiar 'persona' y "desperdiciado"
+# Declarar variables: (p="persona") y (d="deperdiciado")
+p="persona"
+d="desperdiciado"
+print("una \'"+p+"\' que ve la vida a los 50 anios igual que a los 20, ha \""+d+"\" 30 anios de vida")
+
+
+# Ejercicio 08
+texto08="si das pescado a un hombre hambriento lo nutriras durante una jornada. si le ensenias a pescar, le nutriras toda su vida"
+print("Texto 08 :",texto08)
+print("len =",len(texto08))
+# Del texto08, copiar 'nutritras' y "jornada"
+# Declarar variables: (j="jornada") y (n="nutritivas")
+# A partir del punto, seguir en la siguiente linea
+j="jornada"
+n="nutriras"
+print("si das pescado a un hombre hambriento lo \'"+n+"\' durante una \""+j+"\".\nsi le ensenias a pescar, le \'"+n+"\' toda su vida")
+
+
+# Ejercicio 09
+texto09="el respeto se gana, la honestidad se aprecia, la confianza se adquiere y la lealtad se devuelve"
+print("Texto 09 :",texto09)
+print("len =",len(texto09))
+# Del texto09, copiar "honestidad" y 'lealtad'
+# Declarar variables: (h="honestidad") y (l="lealtad")
+h="honestidad"
+l="lealtad"
+print("el respeto se gana, la \""+h+"\" se aprecia, la confianza se adquiere y la \'"+l+"\' se devuelve")
+
+
+# Ejercicio 10
+texto10="aquel que duda y no investiga, se torna no solo infeliz, sino tambien injusto"
+print("Texto 10 :",texto10)
+print("len =",len(texto10))
+# Del texto10, copiar 'investiga' y 'tambien'
+# Declarar variables: (i="investiga") y (t="tambien")
+i="investiga"
+t="tambien"
+print("aquel que duda y no \'"+i+"\', se torna no solo infeliz, sino \'"+t+"\' injusto")
+
+
+# Ejercicio 11
+texto11="pasamos mucho tiempo ganandonos la vida, pero no el suficiente tiempo viviendola"
+print("Texto 11 :",texto11)
+print("len =",len(texto11))
+# Del texto11, copiar "ganandonos" y "viviendola"
+# Declarar variables: (g="ganandonos") y (v="viviendola")
+g="ganandonos"
+v="viviendola"
+print("pasamos mucho tiempo \""+g+"\" la vida, pero no el suficiente tiempo \""+v+"\"")
+
+
+# Ejercicio 12
+texto12="el secreto de la felicidad no esta en hacer lo que a uno le gusta sino en que le guste lo que uno hace"
+print("Texto 12 :",texto12)
+print("len =",len(texto12))
+# Del texto12, copiar 'secreto' y "felicidad"
+# Declarar variables: (s="secreto") y (f="felicidad")
+# A partir de la coma, pasar a la siguiente linea
+s="secreto"
+f="felicidad"
+print("el \'"+s+"\' de la \""+f+"\" no esta en hacer lo que a uno le gusta,\nsino en que le guste lo que uno hace")
+
+
+# Ejercicio 13
+texto13="el mundo pertenece a los optimistas; los pesimistas son meros espectadores"
+print("Texto 13 :",texto13)
+print("len =",len(texto13))
+# Del texto13, copiar 'optimistas' y 'meros espectadores'
+# Declarar variables= (o="optimistas") y (m_e="meros espectadores"
+o="optimistas"
+m_e="meros espectadores"
+print("el mundo pertenece a los \'"+o+"\'; los pesimistas son \'"+m_e+"\'")
+
+
+# Ejercicio 14
+texto14="el chisme emite un veneno triple, porque hiere al que lo dice, al que lo escucha y a la persona de quien se habla"
+print("Texto 14 :",texto14)
+print("len =",len(texto14))
+# Del texto14, copiar "chisme" y "veneno triple"
+# Declarar variables:(ch="chisme") y (v_t="veneno triple")
+# A partir de la coma, pasar a la siguiente linea
+ch="chisme"
+v_t="veneno triple"
+print("el \""+ch+"\" emite un \""+v_t+"\", porque hiere al que lo dice,\nal que lo escucha y a la persona de quien se habla")
+
+
+# Ejercicio 15
+texto15="la felicidad está en la libertad, y la libertad en el coraje"
+print("Texto 15 :",texto15)
+print("len =",len(texto15))
+# Del texto15, copiar "felicidad" y 'libertad'
+# Declarar variables: (fe="felicidad") y (li="libertad")
+fe="felicidad"
+li="libertad"
+print("la \""+fe+"\" está en la \'"+li+"\', y la \'"+li+"\' en el coraje")
