b8c121d062ffe3cb17bbb40d1db40a26ced04027
florespadillaunprg <57303134+florespadillaunprg@users.noreply.github.com>
2019-12-13 09:11:28 -0500

    Se agrego la funcion indexacion (olazabal)

---
+++ olazabal/1_indexacion.py
@@ -0,0 +1,353 @@
+#Ejercicio 01
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#            0         10        20        30
+#            0123456789012345678901234567890123456
+uni="UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[21] + uni[9] + uni[0] + uni[19] + uni[25]
+print(uni[21], uni[9], uni[0], uni[19], uni[25])
+
+
+#Ejercicio 02
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30
+#    0123456789012345678901234567890123456
+uni="UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[14] + uni[4] + uni[14] + uni[15] + uni[19] + uni[29] + uni[33]
+print(uni[14], uni[4], uni[14], uni[15], uni[19], uni[29], uni[33])
+
+
+#Ejercicio 03
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30
+#    0123456789012345678901234567890123456
+uni="UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[21] + uni[22] + uni[23] + uni[24] + uni[25]
+print(uni[21], uni[22], uni[23], uni[24], uni[25])
+
+
+
+#Ejercicio 04
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30
+#    0123456789012345678901234567890123456
+uni="UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[32] + uni[33] + uni[34] + uni[35] + uni[15] + uni[12] + uni[13]
+print(uni[32], uni[33], uni[34], uni[35], uni[15], uni[12], uni[13])
+
+
+
+#Ejercicio 05
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30
+#    0123456789012345678901234567890123456
+uni="UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[9] + uni[19] + uni[2] + uni[13] + uni[12] + uni[30] + uni[33]
+print(uni[9], uni[19], uni[2], uni[13], uni[12], uni[30], uni[33])
+
+
+#Ejercicio 06
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30
+#    0123456789012345678901234567890123456789
+uni="UNIVERSIDAD NACIONAL MAYOR DE SAN MARCOS"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[21] + uni[22] + uni[25] + uni[14] + uni[15] + uni[13] + uni[12] + uni[38] + uni[39]
+print(uni[21], uni[22], uni[25], uni[14], uni[15], uni[13], uni[12], uni[38], uni[39])
+
+
+
+#Ejercicio 07
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30
+#    0123456789012345678901234567890123456789
+uni="UNIVERSIDAD NACIONAL MAYOR DE SAN MARCOS"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[9] + uni[14] + uni[4] + uni[6] + uni[7] + uni[17] + uni[16]
+print(uni[9], uni[14], uni[4], uni[6], uni[7], uni[17], uni[16])
+
+
+#Ejercicio 08
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30
+#    0123456789012345678901234567890123456789
+uni="UNIVERSIDAD NACIONAL MAYOR DE SAN MARCOS"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[14] + uni[16] + uni[21] + uni[0] + uni[1] + uni[2] + uni[37] + uni[9] + uni[5]
+print(uni[14], uni[16], uni[21], uni[0], uni[1], uni[2], uni[37], uni[9], uni[5])
+
+
+
+#Ejercicio 09
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30
+#    0123456789012345678901234567890123456789
+uni="UNIVERSIDAD NACIONAL MAYOR DE SAN MARCOS"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[8] + uni[15] + uni[3] + uni[28] + uni[25] + uni[6] + uni[7] + uni[8] + uni[31] + uni[10]
+print(uni[8], uni[15], uni[3], uni[28], uni[25], uni[6], uni[7], uni[8], uni[31], uni[10])
+
+
+
+#Ejercicio 10
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30
+#    0123456789012345678901234567890123456789
+uni="UNIVERSIDAD NACIONAL MAYOR DE SAN MARCOS"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[8] + uni[15] + uni[3] + uni[28] + uni[25] + uni[6] + uni[7] + uni[8] + uni[31] + uni[10]
+print(uni[8], uni[15], uni[3], uni[28], uni[25], uni[6], uni[7], uni[8], uni[31], uni[10])
+
+
+
+#Ejercicio 11
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012345678
+uni="ME GUSTAS CUANDO CALLAS PORQUE ESTAS COMO AUSENTE"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[10] + uni[15] + uni[19] + uni[7] + uni[3] + uni[31] + uni[13] + uni[38]
+print(uni[10], uni[15], uni[19], uni[7], uni[3], uni[31], uni[13], uni[38])
+
+
+
+#Ejercicio 12
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012345678
+uni="ME GUSTAS CUANDO CALLAS PORQUE ESTAS COMO AUSENTE"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[10] + uni[15] + uni[19] + uni[7] + uni[3] + uni[31] + uni[13] + uni[38]
+print(uni[10], uni[15], uni[19], uni[7], uni[3], uni[31], uni[13], uni[38])
+
+
+
+#Ejercicio 13
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012345678
+uni="ME GUSTAS CUANDO CALLAS PORQUE ESTAS COMO AUSENTE"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[10] + uni[4] + uni[7] + uni[14] + uni[29] + uni[26] + uni[46] + uni[40]
+print(uni[10], uni[4], uni[7], uni[14], uni[29], uni[26], uni[46], uni[40])
+
+
+
+#Ejercicio 14
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012345678
+uni="ME GUSTAS CUANDO CALLAS PORQUE ESTAS COMO AUSENTE"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[6] + uni[1] + uni[2] + uni[9] + uni[7] + uni[0] + uni[40]
+print(uni[6], uni[1], uni[2], uni[9], uni[7], uni[0], uni[40],)
+
+
+
+#Ejercicio 15
+cad1="HOLA MUNDO"
+cad2='HOLA MUNDO'
+cad3="          "
+cad4=''
+
+#print(cad1, len(cad1))
+#print(cad2, len(cad2))
+#print(cad3, len(cad3))
+#print(cad4, len(cad4))
+
+#    0         10        20        30        40
+#    0123456789012345678901234567890123456789012345678
+uni="ME GUSTAS CUANDO CALLAS PORQUE ESTAS COMO AUSENTE"
+
+print(uni[0])   # Acceder al indice 0 -> U
+
+print("len = ",len(uni)) # Imprime la longitud de la cadena uni
+
+#nombre=uni[0] + uni[7] + uni[3] + uni[38] + uni[22]
+print(uni[0], uni[7], uni[3], uni[38], uni[22])
