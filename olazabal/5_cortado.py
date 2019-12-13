b674ffb98e7516d190ec37207d6d6087ef1259d8
florespadillaunprg <57303134+florespadillaunprg@users.noreply.github.com>
2019-12-13 09:13:29 -0500

    Se agrego la funcion cortado (olazabal)

---
+++ olazabal/5_cortado.py
@@ -0,0 +1,130 @@
+#Ejercicio1
+#     0         1         2         3
+#     012345678901234567890123456789012345
+msg1="AL BUEN AMOR, NUNCA LE FALTA QUE DAR"
+print(msg1[8:12]+ " (es diferente de) " + msg1[11:7:-1])                                  #Muestra AMOR (es deiferente de) ROMA
+print(msg1[27:23:-1])                                                                     #Muestra falta al reves
+
+
+
+#Ejercicio2
+#    0         1         2         3         4         5         6
+#    01234567890123456789012345678901234567890123456789012345678901234
+msg2="A QUIEN NO AMA A SUS PARIENTES, DEBERIAN DE ROMPERLE LOS DIENTES"
+print(msg2[53:56]+ "(es diferente de)" + msg2[56:52:-1])                                   #Muestra LOS (es diferente de) SOL
+print(msg2[39:30:-1])                                                                      #Muestra DEBERIAN al reves
+
+
+
+#Ejercicio3
+#     0         1         2         3
+#     012345678901234567890123456789012345
+msg3="A BATALLAS DE AMOR, CAMPOS DE PLUMAS"
+print(msg3[2:10]+ " en " + msg3[20:26])                                                    #Muestra BATALLAS en CAMPOS
+print(msg3[9:1:-1])                                                                        #Muestra BATALLAS al reves
+
+
+
+#Ejercicio4
+#    0         1         2         3         4         5         6
+#    0123456789012345678901234567890123456789012345678901234567890123456789
+msg4="HAY TRES COSAS QUE NO SE PUEDEN OCULTAR: EL HUMO, EL AMOR Y EL DINERO"
+print(msg4[56:52:-1])                                                                       #Muestra AMOR al reves
+print(msg4 [69::-1])                                                                        #Muestra HAY TRES COSAS QUE NO SE PUEDEN OCULTAR: EL HUMO, EL AMOR Y EL DINERO al reves
+
+
+
+#Ejercicio5
+#     0         1         2         3         4
+#     01234567890123456789012345678901234567890123
+msg5="AMOR DE MONJA Y PEDO DE FRAILE, TODO ES AIRE"
+print(msg5[40:])                                                                            #Muestra AIRE
+print(msg5[32:])                                                                            #Muestra TODO ES AIRE
+
+
+
+#Ejercicio6
+#     0         1         2         3
+#     0123456789012345678901234567890123456
+msg6="AL BUENO POR AMOR Y AL MALO POR TEMOR"
+print(msg6[3:17])                                                                           #Muestra BUENO POR AMOR
+print(msg6[36:19:-1])                                                                       #Muestra AL MALO POR TEMOR al reves
+
+
+
+#Ejercicio7
+#    0         1         2
+#    0123456789012345678901234
+msg7="AMOR DE NAVAJA, NO CUAJA"
+print(msg7[13:7:-1])                                                                        #Muestra NAVAJA al reves
+print(msg7[16:])                                                                            #Muestra NO CUAJA
+
+
+
+#Ejercicio8
+#     0         1         2         3
+#     012345678901234567890123456789012345
+msg8="AMOR LOCO, YO POR VOS Y VOS POR OTRO"
+print(msg8[0:9])                                                                            #Muestra AMOR LOCO
+print(msg8[11:21])                                                                          #Muestra YO POR VOS
+
+
+
+#Ejercicio9
+#     0         1         2         3
+#     0123456789012345678901234567890123
+msg9="A QUIEN FEO AMA, HERMOSO LE PARECE"
+print(msg9[8:11]+ " es lo contrario de " + msg9[17:24])                                     #Muestra FEO es lo contrario de HERMOSO
+print(msg9[33:27:-1])                                                                       #Muestra PARECE al reves
+
+
+
+#Ejercicio10
+#      0         1         2
+#      012345678901234567890123456789
+msg10="AMOR COMPRADO,DALO POR PERDIDO"
+print(msg10[5:13] + " o " + msg10[23:])                                                      #Muestra COMPRADO o PERDIDO
+print(msg10[14:])                                                                            #Muestra DALO POR PERDIDO
+
+
+#Ejercicio11
+#      0         1         2         3
+#      012345678901234567890123456789012345678
+msg11="EL CORAZON FUERTE, A LA DESGRACIA VENCE"
+print(msg11[3:10] + " bueno siempre " + msg11[34:])                                          #Muestra CORAZON bueno siempre VENCE
+print(msg11[11:17] + " " + msg11[24:33] )                                                    #Muestra FUERTE DESGRACIA
+
+
+
+#Ejercicio12
+#      0         1         2
+#      01234567890123456789012345
+msg12="A MUCHO AMOR,MUCHO PERDON"
+print(msg12[2:12] + " o " + msg12[19:25])                                                     #Muetra MUCHO AMOR o PERDON
+print(msg12[11:7:-1])                                                                         #Muestra AMOR al reves
+
+
+#Ejercicio13
+#      0         1         2
+#      0123456789012345678901
+msg13="GUERRA EN LAS CALLES"
+print(msg13[12:9:-1] + " EN " + msg13[10:12] + " " + msg13[0:6])                               #Muestra SAL EN LA GUERRA
+print(msg13[21::-1])                                                                           #Muestra GUERRA EN LAS CALLES al reves
+
+
+
+#Ejercicio14
+#     0         1         2         3         4
+#     0123456789012345678901234567890123456789012
+msg14="AMA A QUIEN TE AMA, NO A QUIEN TE ILUSIONA"
+print("se " + msg14[0:4] + "mas no se " + msg14[35:])                                          #Muestra se AMA mas no se ILUSIONA
+print(msg14[42:32:-1])                                                                         #Muestra ILUSIONA al reves
+
+
+
+#Ejercicio15
+#      0         1         2         3         4
+#      012345678901234567890123456789012345678901
+msg15="AMOR POR INTERES, ACABA EN UN DOS POR TRES"
+print("los " + msg15[38:] + " primeros numeros son: " + msg15[27:33] + " " + msg15[38:])       #Muestra los TRES primeros numeros son: UN DOS TRES
+print(msg15[41::-1])                                                                           #Muestra AMOR POR INTERES, ACABA EN UN DOS POR TRES al reves
