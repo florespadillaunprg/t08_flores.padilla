56b0aa41de515af7f7b4f01ff9eaef68b22b8cbd
florespadillaunprg <57303134+florespadillaunprg@users.noreply.github.com>
2019-12-13 09:11:50 -0500

    Se agrego la funcion longitud (olazabal)

---
+++ olazabal/2_longitud.py
@@ -0,0 +1,210 @@
+# Ejercicio 01
+texto01="para torear y casarse, hay que arrimarse"
+print("Texto 01 :",texto01)
+print("len =",len(texto01))
+# Del texto01, copiar "cumplamos" y 'funeraria'
+# Declarar variables: (t="torear") y (a="arrimarse")
+t="torear"
+a="arrimarse"
+print("para ""\""+t+"\" y casarse,hay que \'"+a+"\'")
+
+
+
+
+# Ejercicio 02
+texto02="no hay rosas sin espinas"
+print("len =",len(texto02))
+# Del texto02, copiar 'no' y "espinas"
+# Declarar variables: (a="no") y (e="espinas")
+a="no"
+e="espinas"
+print("\'"+a+"\' hay rosas sin \""+e+"\"")
+
+
+
+
+
+
+# Ejercicio 03
+texto03="donde hay amor no hay pecado"
+print("Texto 03 :",texto03)
+print("len =",len(texto03))
+# Del texto 03, copiar "donde" y 'pecado'
+# Declarar variables: (d="donde") y (p='pecado')
+d="donde"
+p="pecado"
+print("\""+d+"\" hay amor no hay \'"+p+"\'")
+
+
+
+
+
+
+#Ejercicio 04
+texto4="ojos que no ven,corazon que no siente"
+print("Texto 04 :",texto4)
+print("len =",len(texto4))
+# Del texto 04, copiar "ojos" y "no siente"
+# Declarar variables:(o="ojos") y (s="no siente")
+# A partir de la coma, pasar a la siguiente linea
+o="ojos"
+s="no siente"
+print("\""+o+"\" que no ven, corazon que \""+s+"\"")
+
+
+
+
+# Ejercicio 05
+texto05="desgraciado en el juego, afortunado en el juego"
+print("Texto 05 :",texto05)
+print("len =",len(texto05))
+# Del texto05, copiar 'desgraciado' y 'juego'
+# Declarar variables: (d="desgraciado") y (j="juego)
+d="desgraciado"
+j="juego"
+print("\'"+d+"\'""en el juego, afortunado en el \'"+j+"\'")
+
+
+
+
+
+
+# Ejercicio 06
+texto6="todo esta permitido en el amor"
+print("Texto 6 :",texto6)
+print("len =",len(texto6))
+# Del texto6, copiar "esta" y "amor"
+# Declarar variables: (e="esta") y (a="amor")
+e="esta"
+a="amor"
+print("todo \""+e+"\" permitido en el \""+a+"\"")
+
+
+
+
+
+
+# Ejercicio 07
+texto07="ira de enamorados, amores doblados"
+print("Texto 07 :",texto07)
+print("len =",len(texto07))
+# Del texto07, copiar 'enamorados' y "doblados"
+# Declarar variables: (e="enamorados") y (d="doblados")
+# A partir del punto, seguir en la siguiente linea
+e="enamorados"
+d="doblados"
+print("ira de \'"+e+"\' enamorados , amores \""+d+"\"")
+
+
+
+
+
+# Ejercicio 08
+texto08="juras del que ama mujer, no se han de creer"
+print("Texto 08 :",texto08)
+print("len =",len(texto08))
+# Del texto08, copiar 'mujer' y "creer"
+# Declarar variables: (m="mujer") y (c="creer")
+# A partir de la coma, pasar a la siguiente linea
+m="mujer"
+c="creer"
+print("juras del que ama \'"+m+"\' , no se han de \""+c+"\"")
+
+
+
+
+# Ejercicio 09
+texto09="amor de madre, que todo lo demás es aire"
+print("Texto 09 :",texto09)
+print("len =",len(texto09))
+# Del texto09, copiar "madre" y 'demas'
+# Declarar variables: (m="madre") y (d="demas")
+m="madre"
+d="demas"
+print("amor de \""+m+"\", que todo lo \'"+d+"\'es aire")
+
+
+
+
+
+# Ejercicio 10
+texto10="amar es tiempo perdido si no es correspondido"
+print("Texto 10 :",texto10)
+print("len =",len(texto10))
+# Del texto10, copiar 'amar' y "correspondido"
+# Declarar variables: (a="amar") y (c="correspondido")
+# A partir de la coma, pasar a la siguiente linea
+a="amar"
+c="correspondido"
+print("\'"+a+"\' es tiempo perdido si no es \""+c+"\"")
+
+
+
+
+
+# Ejercicio 11
+texto11="el amor entra por los ojos"
+print("Texto 11 :",texto11)
+print("len =",len(texto11))
+# Del texto11, copiar "amor" y "ojos"
+# Declarar variables: (a="amor") y (o="ojos")
+a="amor"
+o="ojos"
+print("el \""+a+"\" entra por los \""+o+"\"")
+
+
+
+
+
+# Ejercicio 12
+texto12="el que con locura ama, nunca llega hasta la cama"
+print("Texto 12 :",texto12)
+print("len =",len(texto12))
+# Del texto12, copiar 'locura' y 'cama'
+# Declarar variables: (l="locura") y (c="cama")
+l="locura"
+c="cama"
+print("el que con \'"+l+"\' ama, nunca llega hasta la \'"+c+"\'")
+
+
+
+
+# Ejercicio 13
+texto13="de enamorado a loco va muy poco"
+print("Texto 13: ", texto13)
+print("len =",len(texto13))
+# Del texto15, copiar "enamorado" y "loco", tambien declararlas
+e="enamorado"
+l="poco"
+print("de \'"+e+"\' a loco va muy \""+ p+"\"")
+
+
+
+
+# Ejercicio 14
+texto14="en cada flor hay color, pero en cualquier amor hay dolor"
+print("Texto 14 :",texto14)
+print("len =",len(texto14))
+# Del texto14, copiar 'flor' y "cualquier"
+# Declarar variables: (f="flor") y (c="cualquier")
+f="flor"
+c="cualquier"
+print("en cada\'"+f+"\' hay color, pero en \""+c+"\" amor hay dolor")
+
+
+
+
+
+# Ejercicio 15
+texto15="ni abril sin flores, ni juventud sin amores"
+print("Texto 15 :",texto15)
+print("len =",len(texto15))
+# Del texto15, copiar 'flores' y "amores"
+# Declarar variables: (f="flores") y (c="amores")
+f="flores"
+c="amores"
+print("ni abril sin \'"+f+"\', ni juventud sin \'"+c+"\'")
+
+
+
+
